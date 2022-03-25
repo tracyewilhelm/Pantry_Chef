686d590ce79b2d98d199d308883adb6c16f17807
Samantha Rexroat <samanthajrexroat@gmail.com>
2022-03-24 19:08:20 -0600

    Joined array to format for params

--- controllers/api/results-routes.js
+++ controllers/api/results-routes.js
@@ -1,12 +1,13 @@
 // GET route for results page
 const axios = require("axios");
 const router = require("express").Router();
+const apiString = require("../../public/js/index.js");
 
 router.get("/", async (req, res) => {
   // Results of user search.
   const recipeData = await axios
     .get(
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=d7b803c21feb41b7b01d22f464050ff6&ingredients=apples,+flour,+sugar,+cinnamon&ranking=1`
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=d7b803c21feb41b7b01d22f464050ff6&ingredients=${apiString}&ranking=1`
     )
     .then((data) => {
       console.log(data.data);
