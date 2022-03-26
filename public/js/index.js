845a8e2964603548d8db16402ac7a99ec170ef47
Tracye Wilhelm <93944059+tracyewilhelm@users.noreply.github.com>
2022-03-25 18:45:03 -0600

    adding notes

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,7 @@
-//define a variable that then
+//define a variable that touches the ingredient form in the html
 const ingredientEl = document.querySelector("#ingredient-form");
 console.log(ingredientEl);
-//in this file you make the api all to spoonacula, once you have that data from your user input you then make your fetch post to you controller route. Once you have that string
-//you then send your data to your fetch
+//in this file you make the api all to spoonacula, once you have that data from your user input you then make your fetch post to you controller route. Once you have that string you then send your data to your new fetch for the actual recipe and instructions
 
 const ingFormHandler = async function (event) {
   event.preventDefault();
@@ -14,7 +13,7 @@ const ingFormHandler = async function (event) {
   $.each(checkedEl, function () {
     selected.push($(this).val());
   });
-
+  //this takes all the values checked and adds a ",+" to the end of each one - this is required formatting for our api url
   const apiString = selected.join(",+");
   console.log(apiString);
 
@@ -25,12 +24,14 @@ const ingFormHandler = async function (event) {
     `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${apiString}&ranking=1&includeInstructions=true`
   );
 
+  //take the data we get back from our api fetch and make it the value of "recipes"
   const recipes = await spoonData.json();
   console.log("recipes line 29");
   console.log(recipes);
-
+  //can we get just the title of index 1?
   console.log(recipes[1].title);
 
+  //take the object we get back and give us just the title and id. Make a new array containing an object for each of the 10 returned recipes with the key/value pairs of title and id
   const summary = recipes.map(({ title, id }) => ({ title, id }));
   console.log(summary);
 
@@ -47,10 +48,10 @@ const ingFormHandler = async function (event) {
   console.log(recipeData);
 
   //recipeData is an array of 10 objects
-
   //    location.replace("/api/results");
 };
 
+//when the user clicks the button, the checked values are turned into a sting, have a ",+" added to the back end, and that string is added where it says apiString. It then sends the api request out
 ingredientEl.addEventListener("submit", ingFormHandler);
 
 // recipes.forEach((recipes.title) => console.log(recipes.title));
